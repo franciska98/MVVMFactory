c091b065ac9a6fa6e09570a928e529e578ea75f8
franciska98 <franciska.miletic8@gmail.com>
2023-03-05 21:25:48 +0100

    Add layout

---
+++ app/src/main/java/com/example/mvvmfactory/ui/ContactsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.mvvmfactory.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mvvmfactory.R
+
+class ContactsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contacts, container, false)
+    }
+}
--- app/src/main/java/com/example/mvvmfactory/ui/MainActivity.kt
+++ app/src/main/java/com/example/mvvmfactory/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mvvmfactory
+package com.example.mvvmfactory.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mvvmfactory.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
