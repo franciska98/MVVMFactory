14f51c3b1e17cbebb11258512c4c1a76c85e063b
franciska98 <franciska.miletic8@gmail.com>
2023-03-12 23:31:37 +0100

    Add validation

--- app/src/main/java/com/example/mvvmfactory/ui/AddOrderFragment.kt
+++ app/src/main/java/com/example/mvvmfactory/ui/AddOrderFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mvvmfactory.ui
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.mvvmfactory.OrderViewModel
+import com.example.mvvmfactory.R
 import com.example.mvvmfactory.data.Order
 import com.example.mvvmfactory.data.TypeOfClothes
 import com.example.mvvmfactory.databinding.FragmentAddOrderBinding
@@ -17,6 +19,7 @@ import com.example.mvvmfactory.databinding.FragmentAddOrderBinding
 class AddOrderFragment : Fragment() {
     private lateinit var binding: FragmentAddOrderBinding
     private val viewModel: OrderViewModel by activityViewModels()
+    private var emptyFields: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,8 +31,27 @@ class AddOrderFragment : Fragment() {
         setSpinner()
 
         binding.addOrderButton.setOnClickListener {
-            viewModel.addOrder(getOrderData())
-            cleanData()
+            validateEmptyEdittext()
+            if (emptyFields == 0) {
+                when {
+                    !validSize(binding.bustEditText.text.toString()) ->
+                        binding.bustEditText.error = getString(R.string.error_invalid_size)
+                    !validSize(binding.weistEditText.text.toString()) ->
+                        binding.weistEditText.error = getString(R.string.error_invalid_size)
+                    !validSize(binding.hipEditText.text.toString()) ->
+                        binding.hipEditText.error = getString(R.string.error_invalid_size)
+                    !validShoulderLength(binding.shoulderLengthEditText.text.toString()) ->
+                        binding.shoulderLengthEditText.error = getString(R.string.error_invalid_size)
+                    !validEmail(binding.contactEmailEditText.text.toString()) ->
+                        binding.contactEmailEditText.error = getString(R.string.error_invalid_email)
+                    !validPhone(binding.contactPhoneEditText.text.toString()) ->
+                        binding.contactPhoneEditText.error = getString(R.string.error_invalid_phone)
+                    else -> {
+                        viewModel.addOrder(getOrderData())
+                        cleanData()
+                    }
+                }
+            }
         }
 
         return binding.root
@@ -44,6 +66,26 @@ class AddOrderFragment : Fragment() {
         )
         binding.typeOfClothesSpinner.adapter = adapterSpinner
     }
+
+    private fun validateEmptyEdittext() {
+        emptyFields = 0
+        binding.dataContainer.children.filterIsInstance<EditText>().forEach {
+            if (it.emptyValidation()) {
+                it.error = getString(R.string.error_empty_field)
+                emptyFields++
+            }
+        }
+    }
+
+    private fun EditText.emptyValidation(): Boolean = text.isNullOrEmpty()
+
+    private fun validSize(size: String): Boolean = (size.length == 2 || size.length == 3)
+
+    private fun validShoulderLength(size: String): Boolean = (size.length == 1 || size.length == 2)
+
+    private fun validEmail(email: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+    private fun validPhone(phone: String): Boolean = phone.length == 10
 
     private fun getOrderData(): Order = Order(
         type = TypeOfClothes.valueOf(binding.typeOfClothesSpinner.selectedItem.toString()),
