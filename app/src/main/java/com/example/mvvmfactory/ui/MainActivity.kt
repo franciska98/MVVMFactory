6fd7acb8203410a2536c97dfbf83b82ac86e7463
franciska98 <franciska.miletic8@gmail.com>
2023-03-17 20:24:41 +0100

    Add toolbar and change colors

--- app/src/main/java/com/example/mvvmfactory/ui/MainActivity.kt
+++ app/src/main/java/com/example/mvvmfactory/ui/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.mvvmfactory.ui
 import android.content.res.Resources
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mvvmfactory.ui.adapter.PageAdapter
+import androidx.appcompat.widget.Toolbar
 import com.example.mvvmfactory.R
 import com.example.mvvmfactory.databinding.ActivityMainBinding
+import com.example.mvvmfactory.ui.adapter.PageAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
@@ -15,17 +16,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar: Toolbar = binding.includeToolbar.toolbar
+        toolbar.title = getString(R.string.app_name)
+        setSupportActionBar(toolbar)
+
         binding.viewPager.adapter = PageAdapter(this)
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, index ->
             tab.text = when (index) {
                 0 -> { getString(R.string.add_person) }
                 1 -> { getString(R.string.contacts) }
-                else -> { throw Resources.NotFoundException("Position not found") }
+                else -> {
+                    throw Resources.NotFoundException("Position not found")
+                }
             }
             tab.icon = when (index) {
                 0 -> { getDrawable(R.drawable.add_icon) }
                 1 -> { getDrawable(R.drawable.list_icon) }
-                else -> { getDrawable(R.drawable.ic_launcher_background) }
+                else -> {
+                    getDrawable(R.drawable.ic_launcher_background)
+                }
             }
         }.attach()
     }
