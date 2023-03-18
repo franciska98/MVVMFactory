01826655ad41f1ce34e000974289094863a51867
franciska98 <franciska.miletic8@gmail.com>
2023-03-18 10:54:11 +0100

    Add data to recycler view

--- app/src/main/java/com/example/mvvmfactory/ui/orders/OrdersFragment.kt
+++ app/src/main/java/com/example/mvvmfactory/ui/orders/OrdersFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.example.mvvmfactory.data.Order
+import com.example.mvvmfactory.data.TypeOfClothes
 import com.example.mvvmfactory.databinding.FragmentOrdersBinding
 import com.example.mvvmfactory.ui.adapter.OrdersRecyclerAdapter
 import com.example.mvvmfactory.ui.viewmodel.OrderViewModel
@@ -24,9 +26,84 @@ class OrdersFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.orderLiveData.observe(viewLifecycleOwner) { contactList ->
-            binding.recyclerView.adapter = OrdersRecyclerAdapter(requireContext(), contactList)
+
+        fillRecyclerView()
+        viewModel.orderLiveData.observe(viewLifecycleOwner) { orderList ->
+            binding.recyclerView.adapter = OrdersRecyclerAdapter(requireContext(), orderList)
         }
+    }
+
+    private fun fillRecyclerView() {
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.SHIRT, TypeOfClothes.SHIRT.image, "98", "65", "100",
+                "12", "Sleeveless", "red", "cotton",
+                "Petra Petric", "petra.petric@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.PULLOVER, TypeOfClothes.PULLOVER.image, "95", "60", "99",
+                "10", "Long sleeve", "blue", "wool",
+                "Sara Petric", "sara.petric@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.BLAZER, TypeOfClothes.BLAZER.image, "80", "65", "99",
+                "15", "Short sleeve", "black", "leather",
+                "Sara Ivic", "sara.ivic@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.BLOUSE, TypeOfClothes.BLOUSE.image, "88", "65", "89",
+                "16", "One sleeve", "pink", "silk",
+                "Lara Ivic", "lara.ivic@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.SHIRT, TypeOfClothes.SHIRT.image, "101", "80", "100",
+                "18", "Short sleeve", "white", "polyester",
+                "Lara Anic", "lara.anic@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.PULLOVER, TypeOfClothes.PULLOVER.image, "88", "65", "89",
+                "16", "Long sleeve", "purple", "wool",
+                "Lara Ivic", "lara.ivic@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.BLOUSE, TypeOfClothes.BLOUSE.image, "80", "65", "99",
+                "15", "One sleeve", "yellow", "silk",
+                "Sara Ivic", "sara.ivic@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.BLAZER, TypeOfClothes.BLAZER.image, "98", "65", "100",
+                "12", "Sleeveless", "black", "polyester",
+                "Petra Petric", "petra.petric@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.SHIRT, TypeOfClothes.SHIRT.image, "87", "70", "90",
+                "15", "One sleeve", "red", "leather",
+                "Ana Anic", "ana.anic@gmail.com", "0951235555",
+            ),
+        )
+        viewModel.addOrder(
+            Order(
+                TypeOfClothes.BLAZER, TypeOfClothes.BLAZER.image, "87", "70", "90",
+                "15", "Long sleeve", "blue", "cotton",
+                "Ana Anic", "ana.anic@gmail.com", "0951235555",
+            ),
+        )
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/mvvmfactory/ui/viewmodel/OrderViewModel.kt
+++ app/src/main/java/com/example/mvvmfactory/ui/viewmodel/OrderViewModel.kt
@@ -6,10 +6,10 @@ import androidx.lifecycle.ViewModel
 import com.example.mvvmfactory.data.Order
 
 class OrderViewModel : ViewModel() {
-    private var _contactsLiveData = MutableLiveData<ArrayList<Order>>(arrayListOf())
-    var orderLiveData: LiveData<ArrayList<Order>> = _contactsLiveData
+    private var _ordersLiveData = MutableLiveData<ArrayList<Order>>(arrayListOf())
+    var orderLiveData: LiveData<ArrayList<Order>> = _ordersLiveData
 
     fun addOrder(order: Order) {
-        _contactsLiveData.value = _contactsLiveData.value.also { it?.add(order) }
+        _ordersLiveData.value = _ordersLiveData.value.also { it?.add(order) }
     }
 }
